6e05c177dc980a9ecad4d8f913fe5bf098e784f5
Cory Todd <cory.todd@canonical.com>
2023-05-19 14:47:48 -0700

    Add AppendAttestationTrailer configurable
    Add opt-in support for appending attestation trailer to commits as part
    of the pull request merge process.
    
    Signed-off-by: Cory Todd <cory.todd@canonical.com>

--- modules/setting/repository.go
+++ modules/setting/repository.go
@@ -86,6 +86,7 @@ var (
 			AddCoCommitterTrailers                   bool
 			TestConflictingPatchesWithGitApply       bool
 			ApproverTrailerToken                     string
+			AppendAttestationTrailers                bool
 		} `ini:"repository.pull-request"`
 
 		// Issue Setting
@@ -212,6 +213,7 @@ var (
 			AddCoCommitterTrailers                   bool
 			TestConflictingPatchesWithGitApply       bool
 			ApproverTrailerToken                     string
+			AppendAttestationTrailers                bool
 		}{
 			WorkInProgressPrefixes: []string{"WIP:", "[WIP]"},
 			// Same as GitHub. See
@@ -227,6 +229,7 @@ var (
 			PopulateSquashCommentWithCommitMessages:  false,
 			AddCoCommitterTrailers:                   true,
 			ApproverTrailerToken:                     "Reviewed-by",
+			AppendAttestationTrailers:                false,
 		},
 
 		// Issue settings
